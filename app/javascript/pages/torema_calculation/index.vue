cf13e08fe22a79f1d2b004d96f0350aa9096d4e0
MKS <mks@MKSnoMacBook-Pro.local>
2022-06-08 18:50:11 +0900

    Add: torema page

---
+++ app/javascript/pages/torema_calculation/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-main>
+    
+  </v-main>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+    }
+  }
+}
+</script>
--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import TopIndex from '../pages/top/index.vue';
+import ToremaCalculation from '../pages/torema_calculation/index.vue';
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ const router = new Router({
       path: '/',
       name: 'TopIndex',
       component: TopIndex
+    },
+    {
+      path: '/torema_calculations',
+      name: 'ToremaCalculation',
+      component: ToremaCalculation,
     }
   ]
 })
