f2cbcd7acca143e2974e85a25caa864a562b017a
MKS <mks@MKSnoMacBook-Pro.local>
2022-07-12 21:59:43 +0900

    list page

---
+++ app/javascript/pages/post/components/PostList.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-expansion-panels popout>
+    <v-expansion-panel
+      v-for="post in posts"
+      :key="post.id"
+      hide-actions
+    >
+      <v-expansion-panel-header>
+        <v-row
+          align="center"
+          class="spacer"
+          no-gutters
+        >
+          <v-col
+            cols="4"
+            sm="2"
+            md="1"
+          >
+            <v-avatar
+              size="36px"
+            >
+              <img
+                alt="Avatar"
+                src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460"
+              >
+            </v-avatar>
+          </v-col>
+
+          <v-col
+            class="hidden-xs-only"
+            sm="5"
+            md="3"
+          >
+            <strong v-html="post.title"></strong>
+            <span
+              v-if="post.description"
+              class="grey--text"
+            >
+              &nbsp;({{ post.description }})
+            </span>
+          </v-col>
+
+          <v-col
+            class="text-no-wrap"
+            cols="5"
+            sm="3"
+          >
+            <v-chip
+              :color="`teal lighten-4`"
+              class="ml-0 mr-2 black--text"
+              label
+              small
+            >
+              Tag
+            </v-chip>
+            <strong v-html="post.title"></strong>
+          </v-col>
+
+          
+        </v-row>
+      </v-expansion-panel-header>
+
+      <v-expansion-panel-content>
+        <v-divider></v-divider>
+        <v-card-text
+          v-text="post.description"></v-card-text>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
---
+++ app/javascript/pages/post/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-subheader>Today</v-subheader>
+
+      <PostList
+        :posts="posts"
+      />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import PostList from "./components/PostList.vue"
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: "PostIndex",
+  components: {
+    PostList
+  },
+  data() {
+    return {
+      noimage_src: require("../../../assets/images/noimage.jpg"),
+      lorem: 'Lorem ipsum dolor sit amet, at aliquam vivendum vel, everti delicatissimi cu eos. Dico iuvaret debitis mel an, et cum zril menandri. Eum in consul legimus accusam. Ea dico abhorreant duo, quo illum minimum incorrupte no, nostro voluptaria sea eu. Suas eligendi ius at, at nemore equidem est. Sed in error hendrerit, in consul constituam cum.',
+    }
+  },
+  computed: {
+    ...mapGetters("posts", ["posts"]),
+  },
+  created() {
+    this.fetchPosts();
+  },
+  methods: {
+    ...mapActions("posts", [
+      "fetchPosts",
+    ])
+  }
+}
+</script>
