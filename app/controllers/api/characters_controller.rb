c89835338571e5c65e5b8a58d3966ba046c7b921
MKS <mks@MKSnoMacBook-Pro.local>
2022-07-19 23:08:26 +0900

    Add: キャラクターAPI #23

---
+++ app/controllers/api/characters_controller.rb
@@ -0,0 +1,20 @@
+class Api::CharactersController < ApplicationController
+  def index
+    character = Character.order(created_at: :desc)
+    render json: character
+  end
+
+  def create
+    character = Character.build(character_params)
+    if character.save
+      render character
+    else
+      render json: character.errors, status: :bad_request
+    end
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :attribute)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resources :my_post
     resources :profile
     resources :tags
+    resources :characters
   end
 
   get '*path', to: 'home#index', constraints: lambda { |req|
